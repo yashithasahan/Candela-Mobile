796886b5c6d9235774b258d0c8e5e4ce5cbb0e5d
Lahiru Imanshana <83973661+Lahiru-Ima@users.noreply.github.com>
2024-01-20 21:50:26 +0530

    Add files

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.amber;
+const kSecondaryColor = Color(0xFFB74D53);
+const kBgColor = Colors.black;
+const kTextColor = Colors.white;
+final kShadowColor = const Color(0xFFD3D3D3).withOpacity(.84);
+
+const kDefaultPadding = EdgeInsets.symmetric(horizontal: 30);
